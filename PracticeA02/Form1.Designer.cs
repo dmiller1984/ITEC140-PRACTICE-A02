a9f5209d20718a5e5efb6760717efdd961da1ee4
dmiller1984 <dalton.miller@mymhc.ca>
2026-09-23 12:26:42 -0600

    Add complete blank WinForms practice project

---
+++ PracticeA02/Form1.Designer.cs
@@ -0,0 +1,35 @@
+namespace PracticeA02;
+
+partial class Form1
+{
+    private System.ComponentModel.IContainer? components = null;
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    private void InitializeComponent()
+    {
+        SuspendLayout();
+        //
+        // Form1
+        //
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(630, 405);
+        Name = "Form1";
+        StartPosition = FormStartPosition.CenterScreen;
+        Text = "PRACTICE ONLY - Concession Register Tax Toggle";
+        ResumeLayout(false);
+    }
+
+    #endregion
+}
+
