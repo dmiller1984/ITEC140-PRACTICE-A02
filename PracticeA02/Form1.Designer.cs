262127ed1fbf6b63221bef9bdf096419e66fea67
Dalton.miller <Dalton.miller@B228-12>
2026-09-24 07:58:56 -0600

    saved repo

--- PracticeA02/Form1.Designer.cs
+++ PracticeA02/Form1.Designer.cs
@@ -17,19 +17,33 @@ partial class Form1
 
     private void InitializeComponent()
     {
+        label1 = new Label();
         SuspendLayout();
-        //
+        // 
+        // label1
+        // 
+        label1.AutoSize = true;
+        label1.Location = new Point(89, 109);
+        label1.Name = "label1";
+        label1.Size = new Size(38, 15);
+        label1.TabIndex = 0;
+        label1.Text = "label1";
+        // 
         // Form1
-        //
+        // 
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
         ClientSize = new Size(630, 405);
+        Controls.Add(label1);
         Name = "Form1";
         StartPosition = FormStartPosition.CenterScreen;
         Text = "PRACTICE ONLY - Concession Register Tax Toggle";
         ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
+
+    private Label label1;
 }
 
